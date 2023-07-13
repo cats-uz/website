8ae33cf84aae7a231e0537ca8b619cee86807e37
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-13 12:59:10 +0000

    server: login router added

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 const express = require("express");
-
 const UserController = require("../controllers/userController");
-const { userValidator } = require("../helpers/validator");
 const router = express.Router();
 const controller = new UserController();
 router.post("/register", controller.register);
+router.post('/login', controller.login)
 module.exports = router;
